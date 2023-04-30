6c23acdc4a855264bcd57020133933497f265613
ajinath72 <ajinathpawar72@gmail.com>
2023-04-30 18:01:25 +0530

    task 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from './components/Layout/Header';
 import React from 'react';
 import './App.css';
+import Meals from './components/Meals/Meals';
 
 const App = () => {
   return (
     <>
       <Header/>
+      <main>
+        <Meals/>
+      </main>
     </>
   )
 }
