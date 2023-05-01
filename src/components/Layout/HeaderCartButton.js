79e3c3830ef4cc3b24e309b105ccfbe4de89a7ac
ajinath72 <ajinathpawar72@gmail.com>
2023-05-01 15:21:29 +0530

    Implementing the cart Itemns page

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import { useContext} from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
+
+
 const HeaderCartButton = (props) => {
+ const cartCtx= useContext(CartContext);
+
+  const numberOfCartItem=cartCtx.items.reduce((curNumber,item)=>{
+    return curNumber+item.amount;
+  },0);
+  
   return (
     <div>
       <button className={classes.button} onClick={props.onClick}>
@@ -12,7 +21,7 @@ const HeaderCartButton = (props) => {
             Your Cart
         </span>
         <span className={classes.badge}>
-            3
+            {numberOfCartItem}
         </span>
     </button>
     </div>
