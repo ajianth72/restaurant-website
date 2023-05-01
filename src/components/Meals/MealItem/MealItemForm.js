5d4850094317e8dba9f4da03a254459edd629d6e
ajinath72 <ajinathpawar72@gmail.com>
2023-05-01 19:43:25 +0530

    DIY - Showing and Enabling the delete of Cart Items

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React,{useRef,useState} from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+
+  const [amountIsValid, setAmountIsValid] =useState(true);
+  const amounntInputRef=useRef();
+  const submitHandler=event=>{
+    event.preventDefault();
+
+  const enteredAmount=amounntInputRef.current.value; 
+  const enteredAmountNumber= +enteredAmount;
+    
+    if(
+      enteredAmount.trim().length ===0||
+      enteredAmountNumber<1 ||
+      enteredAmountNumber>5
+    ){
+      setAmountIsValid(false);
+      return ;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
   return (
     <div>
-        <form className={classes.form}>
-            <Input label="Amount" input={{
+        <form className={classes.form} onSubmit={submitHandler} >
+            <Input
+                ref={amounntInputRef} 
+                label="Amount" 
+                input={{
                 id: 'amount',
                 type: 'number',
                 min:'1',
@@ -15,6 +37,8 @@ const MealItemForm = (props) => {
                 defaultValue:'1'
             }} />
             <button>+Add</button>
+            {!amountIsValid && <p>please enter a valid amount (1-5).</p>}
+            
         </form>
     </div>
   )
